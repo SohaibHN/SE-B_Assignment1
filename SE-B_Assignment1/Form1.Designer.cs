5284896d64fd6f8f623f6414f9e437e3c11cbfd0
sohaib.niwaz <sohaib.niwaz@gmail.com>
2018-02-07 15:48:02 +0000

    Init Git Test & ZedGraph

--- SE-B_Assignment1/Form1.Designer.cs
+++ SE-B_Assignment1/Form1.Designer.cs
@@ -29,11 +29,40 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(2, 2);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(638, 453);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 457);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.SizeChanged += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
     }
 }
 
--- SE-B_Assignment1/Form1.cs
+++ SE-B_Assignment1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZedGraph;
 
 namespace SE_B_Assignment1
 {
@@ -15,6 +16,72 @@ namespace SE_B_Assignment1
         public Form1()
         {
             InitializeComponent();
+            plotGraph();
+            SetSize();
+        }
+
+        private int[] buildTeamAData()
+        {
+            int[] goalsScored = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                goalsScored[i] = (i + 1) * 10;
+            }
+            return goalsScored;
+        }
+
+        private int[] buildTeamBData()
+        {
+            int[] goalsScored = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                goalsScored[i] = (i + 10) * 11;
+            }
+            return goalsScored;
+        }
+
+        private void plotGraph()
+        {
+            GraphPane myPane = zedGraphControl1.GraphPane;
+
+            // Set the Titles
+            myPane.Title.Text = "Team A vs Team B Goal Analysis for 2014/2015 Season";
+            myPane.XAxis.Title.Text = "Year";
+            myPane.YAxis.Title.Text = "No of Goals";
+            /* myPane.XAxis.Scale.MajorStep = 50;
+             myPane.YAxis.Scale.Mag = 0;
+             myPane.XAxis.Scale.Max = 1000;*/
+
+            PointPairList teamAPairList = new PointPairList();
+            PointPairList teamBPairList = new PointPairList();
+            int[] teamAData = buildTeamAData();
+            int[] teamBData = buildTeamBData();
+            for (int i = 0; i < 10; i++)
+            {
+                teamAPairList.Add(i, teamAData[i]);
+                teamBPairList.Add(i, teamBData[i]);
+            }
+
+            LineItem teamACurve = myPane.AddCurve("Team A",
+                   teamAPairList, Color.Red, SymbolType.Diamond);
+
+            LineItem teamBCurve = myPane.AddCurve("Team B",
+                  teamBPairList, Color.Blue, SymbolType.Circle);
+
+            zedGraphControl1.AxisChange();
+        }
+
+        private void SetSize()
+        {
+            zedGraphControl1.Location = new Point(0, 0);
+            zedGraphControl1.IsShowPointValues = true;
+            zedGraphControl1.Size = new Size(this.ClientRectangle.Width - 20, this.ClientRectangle.Height - 50);
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            SetSize();
         }
     }
 }
