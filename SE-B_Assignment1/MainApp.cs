0d72aa421fc84098b8ce25928e34f64e751ecd94
sohaib.niwaz <sohaib.niwaz@gmail.com>
2018-02-14 10:02:54 +0000

    Added: Plotting to ZedGraph with HRData
    To Do:
    Populate Listboxes from HRM File
    Validate HRM File

--- SE-B_Assignment1/MainApp.cs
+++ SE-B_Assignment1/MainApp.cs
@@ -14,10 +14,22 @@ namespace SE_B_Assignment1
 {
     public partial class Form1 : Form
     {
+       /* public string[] speed;
+        public string[] heartrate;
+        public string[] cadence;
+        public string[] altitude;
+        public string[] power;
+        public string[] powerbalance; */
+        List<string> HRSpeed = new List<string>();
+        List<string> heartrate = new List<string>();
+        List<string> cadence = new List<string>();
+        List<string> altitude = new List<string>();
+        List<string> power = new List<string>();
+        List<string> powerbalance = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
-            plotGraph();
            
         }
 
@@ -44,32 +56,54 @@ namespace SE_B_Assignment1
         private void plotGraph()
         {
             GraphPane myPane = zedGraphControl1.GraphPane;
-
+            zedGraphControl1.GraphPane.CurveList.Clear();
+            zedGraphControl1.GraphPane.GraphObjList.Clear();
             // Set the Titles
-            myPane.Title.Text = "Team A vs Team B Goal Analysis for 2014/2015 Season";
-            myPane.XAxis.Title.Text = "Year";
-            myPane.YAxis.Title.Text = "No of Goals";
+            myPane.Title.Text = "";
+            myPane.XAxis.Title.Text = "Time";
+            myPane.YAxis.Title.Text = "Value";
             /* myPane.XAxis.Scale.MajorStep = 50;
              myPane.YAxis.Scale.Mag = 0;
              myPane.XAxis.Scale.Max = 1000;*/
 
-            PointPairList teamAPairList = new PointPairList();
-            PointPairList teamBPairList = new PointPairList();
-            int[] teamAData = buildTeamAData();
-            int[] teamBData = buildTeamBData();
-            for (int i = 0; i < 10; i++)
+            PointPairList HRSpeed1 = new PointPairList();
+            PointPairList HeartRate1 = new PointPairList();
+            PointPairList Cadeance1 = new PointPairList();
+            PointPairList Altitude1 = new PointPairList();
+            PointPairList Power1 = new PointPairList();
+            PointPairList PB1 = new PointPairList();
+
+            int[] Speed = HRSpeed.Select(int.Parse).ToArray();
+            int[] HeartRate = heartrate.Select(int.Parse).ToArray();
+            int[] Altitude = altitude.Select(int.Parse).ToArray();
+            int[] Cadeance = cadence.Select(int.Parse).ToArray();
+            int[] Power = power.Select(int.Parse).ToArray();
+            int[] PB = powerbalance.Select(int.Parse).ToArray();
+
+            for (int i = 0; i < Speed.Length; i++)
             {
-                teamAPairList.Add(i, teamAData[i]);
-                teamBPairList.Add(i, teamBData[i]);
+                HRSpeed1.Add(i, Speed[i]);
+                HeartRate1.Add(i, HeartRate[i]);
+                Altitude1.Add(i, Altitude[i]);
+                Cadeance1.Add(i, Cadeance[i]);
+                Power1.Add(i, Power[i]);
+                PB1.Add(i, PB[i]);
             }
 
-            LineItem teamACurve = myPane.AddCurve("Team A",
-                   teamAPairList, Color.Red, SymbolType.Diamond);
 
-            LineItem teamBCurve = myPane.AddCurve("Team B",
-                  teamBPairList, Color.Blue, SymbolType.Circle);
+            LineItem HeartRateCurve = myPane.AddCurve("Heart Rate",
+                HeartRate1, Color.Blue, SymbolType.Diamond);
+
+            LineItem AltitudeCurve = myPane.AddCurve("Altitude",
+                Altitude1, Color.Green, SymbolType.Diamond);
+
+            LineItem SpeedCurve = myPane.AddCurve("Speed",
+                   HRSpeed1, Color.Red, SymbolType.Diamond);
+
+
 
             zedGraphControl1.AxisChange();
+            zedGraphControl1.Refresh();
         }
 
         private void SetSize()
@@ -91,7 +125,7 @@ namespace SE_B_Assignment1
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Title = "Open Text File";
             ofd.Filter = "hrm|*.hrm";
-            ofd.InitialDirectory = @"C:\";
+            ofd.InitialDirectory = @"D:\Desktop\Work\Uni Last Year\Sem2\SE-B";
             DialogResult result = ofd.ShowDialog();
             if (result == DialogResult.OK) // Test result.
             {
@@ -109,13 +143,28 @@ namespace SE_B_Assignment1
                            .ToList();
 
 
-                    MessageBox.Show(HRData[0]);
+                    string[] Splitter;
+
+                    foreach (var one in HRData)
+                    {
+                        Splitter = one.Split('\t');
+                        heartrate.Add(Splitter[0]);
+                        HRSpeed.Add(Splitter[1]);
+                        cadence.Add(Splitter[2]);
+                        altitude.Add(Splitter[3]);
+                        power.Add(Splitter[4]);
+                        powerbalance.Add(Splitter[5]);
+                        //MessageBox.Show(info13[0]);
+                    }
+                    
+
                 }
                 catch (Exception)
                 {
                     MessageBox.Show("File is incorrect format, please use a correct format", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     //error message on incorrect file types
                 }
+                plotGraph();
             }
         }
     }
